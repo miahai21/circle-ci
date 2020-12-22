aa13052d7756020119fa91bc2b22d42d80a71147
miftodii <miftodii@bmj.com>
2020-12-22 14:18:43 +0000

    fail build

--- src/test/java/com/example/circleci/CircleCiApplicationTests.java
+++ src/test/java/com/example/circleci/CircleCiApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.circleci;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 @SpringBootTest
 class CircleCiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue("", false);
 	}
 
 }
